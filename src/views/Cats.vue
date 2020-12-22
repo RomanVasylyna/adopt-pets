fc2f201d62c71f63fb1514b3e007994dd89c49a4
Roman <romanvasylyna888@gmail.com>
2020-12-22 17:33:27 +0200

    Started Anew

---
+++ src/views/Cats.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Cats</h1>
+
+    </div>
+</template>
+
+
+<script>
+// Importing Cats Array from a file
+// @ directs to a root folder so you don't have to do ../../data etc.
+
+import { component } from 'vue/types/umd'
+
+export default {
+data() {
+return {
+
+}    
+}    
+}
+</script>
